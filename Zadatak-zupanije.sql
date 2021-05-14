315bddc2169c74ae0277401380ce1bcc86a2863c
Josip-github <josipcota1@gmail.com>
2021-05-14 16:01:47 +0200

    Create Zadatak-zupanije.sql

---
+++ Zadatak-zupanije.sql
@@ -0,0 +1,13 @@
+drop database if exists zadatakzupanije;
+create database zadatakzupanije;
+use zadatakzupanije;
+
+/*treba unijeti 3 zupanije
+				6 opcina
+				12 mjesta
+				3 zupana
+				
+	promijeniti 5 mjesta
+	obrisati	2 opcine
+*/
+
